645f40b7df7770b02b155dc10f627f85b064c507
Odin Rochmman <odin@coretrek.no>
2018-05-03 11:10:17 +0200

    updated demo and readme

--- demo/demo.js
+++ demo/demo.js
@@ -3,23 +3,20 @@ import style_light from "../src/theme/light.pcss";
 import style_dark from "../src/theme/dark.pcss";
 import style_c64 from "../src/theme/c64.pcss";
 
-const head = document.head || document.getElementsByTagName('head')[0];
-const addStyle = css => {
-    const style = document.createElement('style');
-    style.type = "text/css";
-    if (style.styleSheet){
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-    head.appendChild(style);
-}
-
-addStyle(`
+const css = `
     codin-codeblock.light{${style_light}}
     codin-codeblock.dark{${style_dark}}
     codin-codeblock.c64{${style_c64}}
-`);
+`;
+const head = document.head || document.getElementsByTagName('head')[0];
+const style = document.createElement('style');
+style.type = "text/css";
+if (style.styleSheet){
+    style.styleSheet.cssText = css;
+} else {
+    style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
 
 require("prismjs/components/prism-markup-templating.js");
 require("prismjs/components/prism-php.js");
--- readme.md
+++ readme.md
@@ -99,23 +99,20 @@ import style_light from "@codin/html-codeblock/src/theme/light.pcss";
 import style_dark from "@codin/html-codeblock/src/theme/dark.pcss";
 import style_c64 from "@codin/html-codeblock/src/theme/c64.pcss";
 
-const head = document.head || document.getElementsByTagName('head')[0];
-const addStyle = css => {
-    const style = document.createElement('style');
-    style.type = "text/css";
-    if (style.styleSheet){
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-    head.appendChild(style);
-}
-
-addStyle(`
+const css = `
     codin-codeblock.light{${style_light}}
     codin-codeblock.dark{${style_dark}}
     codin-codeblock.c64{${style_c64}}
-`);
+`;
+const head = document.head || document.getElementsByTagName('head')[0];
+const style = document.createElement('style');
+style.type = "text/css";
+if (style.styleSheet){
+    style.styleSheet.cssText = css;
+} else {
+    style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
 ```
 
 @todo
