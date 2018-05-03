916f1340d7765553d51daf0c41717c4495691383
Odin Rochmman <odin@coretrek.no>
2018-05-03 19:15:14 +0200

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,23 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.(pcss)$/,
-        use: "text-loader",
+        use: "text-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
       }
     ]
   }
